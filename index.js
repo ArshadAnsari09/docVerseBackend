0ac110466053c7eb993ad0aa3685c614c955875e
Arshad Ansari <arshad.ansari.e21@nsut.ac.in>
2024-10-27 19:54:23 +0530

    intial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const Document = require("./database/Documents");
+
+mongoose
+  .connect("mongodb://localhost/google-docs-clone")
+  .then(() => console.log("Connected!"));
+
+const io = require("socket.io")(4000, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+const defaultValue = "";
+
+io.on("connection", (socket) => {
+  socket.on("get-document", async (documentId) => {
+    const document = await findOrCreateDocument(documentId);
+    socket.join(documentId);
+    socket.emit("load-document", document.data);
+
+    socket.on("send-changes", (delta) => {
+      socket.broadcast.to(documentId).emit("receive-changes", delta);
+    });
+
+    socket.on("save-document", async (data) => {
+      await Document.findByIdAndUpdate(documentId, { data });
+    });
+  });
+});
+
+async function findOrCreateDocument(id) {
+  if (id == null) return;
+
+  const document = await Document.findById(id);
+  if (document) return document;
+  return await Document.create({ _id: id, data: defaultValue });
+}
